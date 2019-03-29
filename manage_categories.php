25a1ff5ac63522c4b263d3fb464ccda1b8189669
lewismcd1 <lewis_432@live.co.uk>
2019-03-29 21:26:51 +0000

    Update manage_categories.php

--- manage_categories.php
+++ manage_categories.php
@@ -39,18 +39,7 @@ $last_login = $_SESSION["last_login"];
                 <th>Action</th>
             </tr>
             </thead>
-            <tbody id="get_category">
-           <!-- <tr>
-                <td>1</td>
-                <td>Electronics</td>
-                <td>Root</td>
-                <td><a href="#" class="btn btn-success btn-sm">Active</a> </td>
-                <td>
-                    <a href="#" class="btn btn-danger btn-sm">Delete</a>
-                    <a href="#" class="btn btn-info btn-sm">Edit</a>
-                </td>
-            </tr>-->
-            </tbody>
+            <tbody id="get_category"></tbody>
         </table>
     </div>
 </div>
